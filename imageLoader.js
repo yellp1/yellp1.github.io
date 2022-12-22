a097c04dfcd55d3b6bc9b5f2bb7e844fc84d2e3d
yellp_a_delp <43158393+yellp1@users.noreply.github.com>
2022-12-22 10:03:13 -0700

    Update imageLoader.js

--- imageLoader.js
+++ imageLoader.js
@@ -5,7 +5,7 @@ $.ajax({
     success: function (data) {
         $(data).find("a").attr("href", function (i, val) {
             if( val.match(/\.(jpe?g|png|gif)$/) ) { 
-                $(".image-container").append( "<img src='" + val +"'>" );
+                $(".image-container").append("<img src='" + val +"'>");
             } 
         });
     }
