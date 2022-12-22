e54c4b7c8110ed6830e31e53645055e5e11b4d6e
yellp_a_delp <43158393+yellp1@users.noreply.github.com>
2022-12-22 10:19:16 -0700

    Update imageLoader.js

--- imageLoader.js
+++ imageLoader.js
@@ -1,12 +1,19 @@
-var folder = "images/";
-
-$.ajax({
-    url : folder,
-    success: function (data) {
-        $(data).find("a").attr("href", function (i, val) {
-            if( val.match(/\.(jpe?g|png|gif)$/) ) { 
-                $(".image-container").append("<img src='" + val +"'>");
-            } 
-        });
-    }
-});
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "/images", true);
+xhr.responseType = 'document';
+xhr.onload = () => {
+  if (xhr.status === 200) {
+    var elements = xhr.response.getElementsByTagName("a");
+    for (x of elements) {
+      if ( x.href.match(/\.(jpe?g|png|gif)$/) ) { 
+          let img = document.createElement("img");
+          img.src = x.href;
+		  $(".image-container").append(img);
+      } 
+    };
+  } 
+  else {
+    alert('Request failed. Returned status of ' + xhr.status);
+  }
+}
+xhr.send()
