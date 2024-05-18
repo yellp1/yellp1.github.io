1ede6c68cd6faf1304f45228d9dcf10631a9470b
yellp_a_delp <43158393+yellp1@users.noreply.github.com>
2024-05-18 02:59:27 -0700

    Adding sidebar element.

--- scripts/mainjavascript.js
+++ scripts/mainjavascript.js
@@ -1,7 +1,35 @@
-$(function(){
-	$("#navigation div a").on("click", function(){
-		$("#navigation div a").removeClass("selected")
-		$(this).addClass("selected")
-		return true;
-	})
-})
+// Get the sidebar button element
+const sidebarButton = document.querySelector('#topbar-navigation > .sidebar-button > a');
+const sidebar = document.querySelector('#sidebar-navigation');
+
+const page = document.querySelector('#page');
+
+// Create a cover div
+const fullscreenCover = document.createElement('div');
+fullscreenCover.id = 'fullscreen-cover';
+page.appendChild(fullscreenCover);
+
+
+function _setSidebar(value) {
+	const state = sidebar.classList.toggle('isOpened', value);
+
+	// Set the cover div to our sidebar state.
+	fullscreenCover.classList.toggle('active', state);
+}
+
+function _toggleSidebar() { _setSidebar(); }
+
+// Add a click event listener to the sidebar button
+sidebarButton.addEventListener('click', _toggleSidebar);
+
+// Add a click event listener to each element in the sidebarr
+const sidebarElements = document.querySelectorAll('#sidebar-navigation > .navigation-button > a');
+
+sidebarElements.forEach(function(element) {
+	element.addEventListener('click', _toggleSidebar);
+});
+
+// Add a click event listener to the cover div
+fullscreenCover.addEventListener('click', function() {
+	_setSidebar(false);
+});
